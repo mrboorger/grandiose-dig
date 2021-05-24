aef49fd4302854047919c1bdf49894a926c5947e
mrboorger <lolov.91@bk.ru>
2021-05-24 21:56:08 +0300

    Fixed some issues.

--- view/block_drawer.cpp
+++ view/block_drawer.cpp
@@ -13,9 +13,18 @@ namespace BlockDrawer {
 namespace {
 
 const std::array<QString, Block::kFrontTypesCount> kFrontNames = {
-    "",         "dirt.png",        "grass.png",        "snowy_grass.png",
-    "sand.png", "sandstone.png",   "stone.png",        "coal_ore.png",
-    "iron_ore", "shimond_ore.png", "firemond_ore.png", "technical.png",
+    "",
+    "dirt.png",
+    "grass.png",
+    "stone.png",
+    "snowy_grass.png",
+    "sand.png",
+    "sandstone.png",
+    "coal_ore.png",
+    "iron_ore",
+    "shimond_ore.png",
+    "firemond_ore.png",
+    "technical.png",
     "torch.png"};
 
 const std::array<QString, kBackgroundsCount> kBackgroundNames = {
